3491909fbd4308334b60081340a9863c801037c4
vagrant <vagrant@localhost.localdomain>
2020-06-01 11:25:04 +0000

    update

--- app/controllers/members/orders_controller.rb
+++ app/controllers/members/orders_controller.rb
@@ -1,20 +1,21 @@
 class Members::OrdersController < Members::BaseController
-def index
-end
+	def index
+	end
 
-def show
-end
+	def show
+	end
 
-def confilm
-end
+	def confilm
+		
+	end
 
-def new
-end
+	def new
+	end
 
-def create
-end
+	def create
+	end
 
-def complete
-end
+	def complete
+	end
 
 end
