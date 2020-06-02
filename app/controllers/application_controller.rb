1ded9b74b3186946f13296e39760b07c506bcad5
vagrant <vagrant@localhost.localdomain>
2020-06-02 08:53:07 +0000

    ジャンルの登録・一覧画面,編集画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    owner_home_top_path
-  end
+    def after_sign_in_path_for(resource)
+        case resource
+        when Owner
+            owner_home_top_path
+        when Member
+            root_path
+        end
+    end
 
-  def after_sign_out_path_for(resource)
-    new_owner_session_path
-  end
+    def after_sign_out_path_for(resource)
+        case resource
+        when :owner
+            new_owner_session_path
+        when :member
+            root_path
+        end
+    end
+
+    def after_sign_up_path_for(resource)
+        case resource
+        when Member
+            root_path
+        end
+    end
 end
