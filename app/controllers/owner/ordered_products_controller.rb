756eca630fb0675300a5c09ad6d4c490800095a4
vagrant <vagrant@localhost.localdomain>
2020-06-04 06:51:16 +0000

    owner_order_show

--- app/controllers/owner/ordered_products_controller.rb
+++ app/controllers/owner/ordered_products_controller.rb
@@ -1,5 +1,32 @@
 class Owner::OrderedProductsController < Owner::BaseController
+before_action :authenticate_owner!
+
 def update
-end
+	@order = Order.find(params[:id])
+	@ordered_products = OrderedProduct.find(params[:abc])
+	@ordered_products.update(ordered_products_params)
+	@orderedproducts = @order.ordered_products
+
+
+	if @orderedproducts.find_by(production_status: "製作中")
+	@order.update(status: "製作中")
+	redirect_to owner_order_path(@order.id)
+	elsif a = 0
+	@orderedproducts.each do |order_product|
+	  if ordered_products.production_status == "制作完了"
+	    a += 1
+	  end
+	end
+	if a == @orderedproducts.count
+	  @order.update(status: "発送準備中")
+	  redirect_to owner_order_path(@order.id)
+	end
+	end
+	end
+
+	def order_items_params
+	params.require(:order_item).permit(:production_status)
+	end
+	end
 
 end
--- app/controllers/owner/orders_controller.rb
+++ app/controllers/owner/orders_controller.rb
@@ -1,12 +1,30 @@
 class Owner::OrdersController < Owner::BaseController
 def index
-@products = Product.all
+	@products = Product.all
 end
 
 def show
+	@order = Order.find(params[:id])
 end
 
 def update
+	@order = Order.find(params[:id])
+	@order.update(orders_params)
+	@ordered_products = @order.ordered_products.all
+	if @order.status == "入金待ち"
+	@ordered_products.each do |ordered_product|
+	order_product.update(production_status: :着手不可)
+	end
+	redirect_to owner_orders_path(@order.id)
+	else
+	render :show
+	end
+end
+
+private
+
+def orders_params
+	params.require(:order).permit(:status)
 end
 
 end
