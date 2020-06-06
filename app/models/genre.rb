7f2417364caa1195c3529015ada8e6d3770f6fc6
vagrant <vagrant@localhost.localdomain>
2020-06-06 06:36:35 +0000

    空コミット

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ApplicationRecord
     has_many :products
     validates :name, presence: true
+    enum effective_status: {true: true, false: false}
+  validates :genre ,presence: true
+  validates :effective_status, presence: true
 end
