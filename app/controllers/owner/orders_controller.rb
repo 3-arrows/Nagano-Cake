d2c448857f174b499ce2a98b7dbf3142f0f599d1
vagrant <vagrant@localhost.localdomain>
2020-06-11 10:05:15 +0000

    fix

--- app/controllers/owner/orders_controller.rb
+++ app/controllers/owner/orders_controller.rb
@@ -2,26 +2,18 @@ class Owner::OrdersController < Owner::BaseController
 	before_action :authenticate_owner!
 	def index
 		@orders = Order.all
-		@ordered_products = @orders.ordered_products.all
 	end
 
 	def show
 		@order = Order.find(params[:id])
-		@ordered_product = @order.ordered_product
+		@order.postage = POSTAGE_PRICE
 	end
 
 	def update
 		@order = Order.find(params[:id])
 		@order.update(orders_params)
-		@ordered_products = @order.ordered_products.all
-		if @order.status == "入金待ち"
-		   @ordered_products.each do |ordered_product|
-		   order_product.update(production_status: :着手不可)
-		   end
-		   redirect_to owner_orders_path(@order.id)
-		else
-		   render :show
-		end
+		@ordered_products = @order.ordered_products
+		redirect_to owner_order_path(@order)
 	end
 
 	private
