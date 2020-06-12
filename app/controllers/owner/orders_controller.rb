23be94c7f96d1ce6d18cc1f91e4246387c49e887
keisuke kamioka <keee2404@gmail.com>
2020-06-12 03:08:49 +0000

    [update]

--- app/controllers/owner/orders_controller.rb
+++ app/controllers/owner/orders_controller.rb
@@ -1,17 +1,23 @@
 class Owner::OrdersController < Owner::BaseController
-	before_action :authenticate_owner!
+	before_action :authenticate_owner! #未ログイン時、閲覧不可
 	def index
 		@orders = Order.all
 	end
 
 	def show
 		@order = Order.find(params[:id])
-		@order.postage = POSTAGE_PRICE
+		@order.postage = POSTAGE_PRICE #POSTAGE_PRICE = application_controllerにて¥800定義済み
 	end
 
 	def update
 		@order = Order.find(params[:id])
 		@order.update(orders_params)
+		if @order.status == '入金確認'
+			@order.ordered_products.each do |ordered_product|
+				ordered_product.production_status = '製作待ち'
+				ordered_product.save
+			end
+		end
 		@ordered_products = @order.ordered_products
 		redirect_to owner_order_path(@order)
 	end
